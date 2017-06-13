bd0a793136b9da45b454b9fdaec4e05e73551342
incrementumio <fjadoon@incrementum.io>
2017-06-14 02:30:30 +0500

    Create script.js

--- Connexiz/assets/js/script.js
+++ Connexiz/assets/js/script.js
@@ -24,11 +24,11 @@ $('.arrow').click(function () {
     if(window.location.href.indexOf("margin_share") > -1 || window.location.href.indexOf("immigration") > -1 ) {
        
  
-	$('#corp b').css({'margin-top':'-63px','transition-delay':'0.5s' });
+	$('#corp b').css({'margin-top':'3.5','transition-delay':'0.5s' });
 
   
   }else{
-	  $('#corp b').css({'margin-top':'3.5px','transition-delay':'1.5s' });
+	  $('#corp b').css({'margin-top':'-63px','transition-delay':'1.5s' });
 
 
 
